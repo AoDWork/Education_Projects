977738946a5fc81832e00253dee637cc27f3a984
AoD <example@gmail.com>
2022-01-05 13:47:54 +0200

    Practise 1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 "use strict";
-let category = "toys";
-console.log(`url//${category}/5`);
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres:[],
+    privat:false
+};
+
+let nameOfFilm1 = prompt("Последний просмотренный фильм?", "");
+let rate1 = prompt("Оцените этот фильм.", "");
+let nameOfFilm2 = prompt("Последний просмотренный фильм?", "");
+let rate2 = prompt("Оцените этот фильм.", "");
+personalMovieDB.movies[nameOfFilm1] = rate1;
+personalMovieDB.movies[nameOfFilm2] = rate2;
+console.log(personalMovieDB);
