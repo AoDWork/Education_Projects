71e62cc7f137f82c9cf1f6560de80ed97e579361
AoD <example@gmail.com>
2022-01-05 15:43:33 +0200

    Practise 2 Cycles

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 "use strict";
-let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
 let personalMovieDB = {
     count: numberOfFilms,
@@ -9,10 +9,26 @@ let personalMovieDB = {
     privat:false
 };
 
-let nameOfFilm1 = prompt("Последний просмотренный фильм?", "");
-let rate1 = prompt("Оцените этот фильм.", "");
-let nameOfFilm2 = prompt("Последний просмотренный фильм?", "");
-let rate2 = prompt("Оцените этот фильм.", "");
-personalMovieDB.movies[nameOfFilm1] = rate1;
-personalMovieDB.movies[nameOfFilm2] = rate2;
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Последний просмотренный фильм?", "");
+    let b = prompt("Оцените этот фильм.", "");
+
+    if(a != null && b != null && a != "" && b != "" && a.length < 50){
+        personalMovieDB.movies[a] = b;
+        console.log("Job's Done");
+    }else{
+        console.log("Error");
+        i--;
+    }
+}
 console.log(personalMovieDB);
+
+if(personalMovieDB.count < 10){
+    document.write("Просмотрено мало фильмов");
+}else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    document.write("Вы классический зритель");
+}else if(personalMovieDB.count >= 30){
+    document.write("Вы киноман");
+}else{
+    document.write("Произошла ошибка");
+}
