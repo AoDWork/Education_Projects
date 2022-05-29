0a1a73a57c07c9954619c4d98dec18e80ad23ed8
AoD <Aodtest07@gmail.com>
2022-05-29 13:28:16 +0300

    009 Practice done

--- Comments_Udemy_course.js
+++ Comments_Udemy_course.js
@@ -782,13 +782,34 @@
     movies[keyMovie];    - такой метод позволит избежать багов при работе с кирилицей(*если значение ключа динамическое)
 
     ***Проверить что бы все работало без ошибок в консоли.  */
+   "use strict";
+   const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+   
+   const personalMovieDB = {
+       count: numberOfFilms,
+       movies: {},
+       actors: {},
+       genres: [],
+       privat: false
+   };
+   
+   const movieName = prompt('Один из последних просмотренных фильмов?', ''),
+         movieRank = prompt('На сколько его оцените?', ''),   
+         movieName2 = prompt('Один из последних просмотренных фильмов?', ''),
+         movieRank2 = prompt('На сколько его оцените?', '');
+   
+   personalMovieDB.movies[movieName] = movieRank;
+   personalMovieDB.movies[movieName2] = movieRank2;
+   
+   
+   console.log(personalMovieDB.movies);
+   console.log(typeof(personalMovieDB.movies));
 
-    const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+}
 
+{//  010    ==== Условия  ====
 
-
-
-
+    
 
 }
 
--- js/practice.js
+++ js/practice.js
@@ -2,25 +2,21 @@
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
-    count: 0,
+    count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
     privat: false
 };
 
-let movieName = prompt('Один из последних просмотренных фильмов?', '');
-let movieRank = +prompt('На сколько его оцените?', '');
+const movieName = prompt('Один из последних просмотренных фильмов?', ''),
+      movieRank = prompt('На сколько его оцените?', ''),   
+      movieName2 = prompt('Один из последних просмотренных фильмов?', ''),
+      movieRank2 = prompt('На сколько его оцените?', '');
 
-let movieName2 = prompt('Один из последних просмотренных фильмов?', '');
-let movieRank2 = +prompt('На сколько его оцените?', '');
-
-personalMovieDB.count = numberOfFilms;
 personalMovieDB.movies[movieName] = movieRank;
 personalMovieDB.movies[movieName2] = movieRank2;
 
 
 console.log(personalMovieDB.movies);
 console.log(typeof(personalMovieDB.movies));
-//document.write(`Фильмов просмотрено ${personalMovieDB.count}`);
-document.write(personalMovieDB.movies);
